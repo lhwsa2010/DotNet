16e846353c2c891175ac214cc8a697f854c699f2
LA <754240664@qq.com>
2021-07-26 22:12:28 +0800

    update ExtObject

--- src/DotNet/DataType/ExtObject.cs
+++ src/DotNet/DataType/ExtObject.cs
@@ -11,69 +11,17 @@ namespace System
     public static class ExtObject
     {
         /// <summary>
-        /// object to string (如果为null值则返回"")
+        /// object to string (if object is null return string empty)
         /// </summary>
         /// <param name="s"></param>
         /// <returns></returns>
         public static string GetString(this object s)
         {
             if ((Object.Equals(s, null)) || (Object.Equals(s, System.DBNull.Value)))
-            {
                 return "";
-            }
-            else
-            {
-                return s.ToString();
-            }
+            return s.ToString();
         }
 
-        ///// <summary>
-        ///// object to int (如果为null值则返回0)
-        ///// </summary>
-        ///// <param name="s"></param>
-        ///// <returns></returns>
-        //public static int GetInt(this object s)
-        //{
-        //    return s.GetString().GetInt();
-        //}
-        ///// <summary>
-        ///// object to float (如果为null值则返回0)
-        ///// </summary>
-        ///// <param name="s"></param>
-        ///// <returns></returns>
-        //public static float GetFloat(this object s)
-        //{
-        //    return s.GetString().GetFloat();
-        //}
 
-        ///// <summary>
-        ///// object to decimal (如果为null值则返回0)
-        ///// </summary>
-        ///// <param name="s"></param>
-        ///// <returns></returns>
-        //public static decimal GetDecimal(this object s)
-        //{
-        //    return s.GetString().GetDecimal();
-        //}
-
-        ///// <summary>
-        ///// object to double (如果为null值则返回0)
-        ///// </summary>
-        ///// <param name="s"></param>
-        ///// <returns></returns>
-        //public static double GetDouble(this object s)
-        //{
-        //    return s.GetString().GetDouble();
-        //}
-
-        ///// <summary>
-        ///// object to Datetime (如果为null值则返回当前时间)
-        ///// </summary>
-        ///// <param name="s"></param>
-        ///// <returns></returns>
-        //public static DateTime GetDateTime(this object s)
-        //{
-        //    return s.GetString().GetDateTime();
-        //}
     }
 }
