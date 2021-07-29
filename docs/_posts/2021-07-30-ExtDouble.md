ce750a9de6888483895df87fc79b4663cdc5bb37
LA <754240664@qq.com>
2021-07-30 01:07:04 +0800

    update ExtDouble

---
+++ docs/_posts/2021-07-30-ExtDouble.md
@@ -0,0 +1,23 @@
+---
+title: ExtDouble
+author: 骆宏伟
+date: 2021-07-30
+category: DataType
+layout: post
+---
+
+## Method
+
+#### ExtDouble
+convert nullable double to double
+```
+double? d=null;
+Console.WriteLine(d.GetDouble());
+d=0.1D;
+Console.WriteLine(d.GetDouble());
+```
+output:
+```
+0
+0.1
+```
--- src/DotNet/DataType/ExtDouble.cs
+++ src/DotNet/DataType/ExtDouble.cs
@@ -11,18 +11,13 @@ namespace System
     public static class ExtDouble
     {
         /// <summary>
-        /// 可空double类型转换为double.
+        /// convert nullable double to double
         /// </summary>
         /// <param name="d"></param>
         /// <returns></returns>
         public static double GetDouble(this double? d)
         {
-            double s = 0;
-            if (d.HasValue)
-            {
-                double.TryParse(d.GetString(), out s);
-            }
-            return s;
+            return d ?? default;
         }
     }
 }
