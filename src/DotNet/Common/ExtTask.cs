d3a8e44fc6db3786493940a726a3e5ae4081eb5d
LA <754240664@qq.com>
2021-08-23 16:08:50 +0800

    Add ExtTask

---
+++ src/DotNet/Common/ExtTask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Threading.Tasks
+{
+    public static class ExtTask
+    {
+        /// <summary>
+        /// Set the task with the special timeout.
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="task"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public static async Task<TResult> WaitTimeAsync<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            using (var timeoutCancellationTokenSource = new CancellationTokenSource())
+            {
+                var delayTask = Task.Delay(timeout, timeoutCancellationTokenSource.Token);
+                if (await Task.WhenAny(task, delayTask) == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    return await task;
+                }
+                throw new TimeoutException("The operation has timed out.");
+            }
+        }
+
+        /// <summary>
+        /// Set the task with the special timeout.
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public static async Task WaitTimeAsync(this Task task, TimeSpan timeout)
+        {
+            using (var timeoutCancellationTokenSource = new CancellationTokenSource())
+            {
+                var delayTask = Task.Delay(timeout, timeoutCancellationTokenSource.Token);
+                if (await Task.WhenAny(task, delayTask) == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    await task;
+                }
+                throw new TimeoutException("The operation has timed out.");
+            }
+        }
+    }
+}
